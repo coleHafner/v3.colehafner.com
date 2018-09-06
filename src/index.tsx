401f5f73652f8d7bda0576c5f8f02cc7b5fca4c8
Cole Hafner <colehafner@Coles-MBP.home>
2018-09-06 09:24:01 -0700

    initial commit.

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld';
+import './styles.css';
+
+ReactDOM.render(
+	<HelloWorld />,
+	document.getElementById('root'),
+);
